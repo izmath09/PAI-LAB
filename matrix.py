803fb129d8b0d628db4903d540673935751d71c4
izmath09 <izmafathima24@gmail.com>
2024-06-06 21:11:15 +0530

    Create matrix.py

---
+++ matrix.py
@@ -0,0 +1,5 @@
+N = 4
+print("The dimension : " + str(N))
+res = [list(range(1 + N * i, 1 + N * (i + 1)))
+                            for i in range(N)]
+print("The created matrix of N * N: " + str(res))
